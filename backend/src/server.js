bf16e9690f587fd901ae88632c75bf120f438cdb
BT01257 <devdwuti@gmail.com>
2025-10-21 23:21:19 +0530

    add: dashboard_route

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,13 @@ app.get("/" , (req,res) => {
     return res.json({msg : "done bai"}) 
 } )
 
- 
+
+app.get("/dashboard", (req,res)=>{
+    return res.json({msg : "dashboard h vai"}) 
+})
+
+
+
 app.listen(port , (req,res)=> {
     console.log("chal rha bhai");
 })
