7f67ba1d2243b6a163293ddf39c90578b5cad469
BT01257 <devdwuti@gmail.com>
2025-10-23 12:54:12 +0530

    Test: question_schema tested

--- backend/models/questions.js
+++ backend/models/questions.js
@@ -7,17 +7,24 @@ import relativeTime from "dayjs/plugin/relativeTime.js";
 dayjs.extend(utc);
 dayjs.extend(relativeTime);
 
-const questionSchema = new Schema({
+const difficultyLevels = {
+  0: "Absolute_clueless",
+  1: "Vague_idea",
+  2: "did_it_with_Little_help",
+  3: "almost_did_clean",
+  4: "smooth_af"
+};
+
+const questionSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true},
   note: { type: String, lowercase: true, trim: true},
-  difficulty: { type: String,
-     enum: ["EasyEasy","EasyMedium","EasyHard", "MediumEasy","MediumMedium","MediumHard","HardEasy","HardMedium","HardHard"], required: true 
+  difficulty: { type: Number,
+     enum: [0, 1, 2, 3, 4], required: true 
     },
   reviewdate: { type: Date, default: Date.now },
   repetitions: { type: Number, default: 0 },
   interval: { type: Number, default: 1 },
   isMastered: { type: Boolean, default: false },  //delete karva do
-
 });
 
 questionSchema.virtual("lastReviewedAgo").get(function () {
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,32 @@
 import express from "express"
-import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import {connectDB , sample_user_data, mockQuestions} from "./db.js";
+import Accounts from '../models/Account.js';
+import Questions from "../models/questions.js";
+
+
+dotenv.config();
+
+
 const app = express();
 const port = 3000;
 
 
-
-
-async function runGetStarted() {
-  // Replace the uri string with your connection string
-  const uri = process.env.MONGO_KEY;
-  const client = new MongoClient(uri);
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Queries for a movie that has a title value of 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    await client.close();
-  }
-}
-runGetStarted().catch(console.dir);
-
 app.get("/" , (req,res) => {
     return res.json({msg : "done bai"}) 
 } )
+
+
+app.get("/sample_data" , async(req,res) => {
+  try{
+    const result = await Questions.create(mockQuestions);
+    console.log("data inserted");
+    res.json({msg:"data inserted", data:result});
+  }
+  catch(err){
+    res.json({msg:err.message});
+  }
+  })
 
 
 app.get("/dashboard", (req,res)=>{
@@ -34,5 +35,6 @@ app.get("/dashboard", (req,res)=>{
 
 
 app.listen(port , (req,res)=> {
-    console.log("chal rha bhai");
+    console.log("running")
+    connectDB();
 })
