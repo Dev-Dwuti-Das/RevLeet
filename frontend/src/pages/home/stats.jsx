2ac43592c10014f5eb744fa603c96139630095db
BT01257 <devdwuti@gmail.com>
2025-11-24 22:20:44 +0530

    add: 70 default questions

--- frontend/src/pages/home/stats.jsx
+++ frontend/src/pages/home/stats.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function stats() {
     return (  
         <>
-        <div className='flex-auto border-2 rounded-xl p-10'>
+        <div className='flex-auto border-2 rounded-2xl p-10'>
             <h1 className='text-3xl font-bold'>Streak</h1>
             <h2>3 days </h2>
 
