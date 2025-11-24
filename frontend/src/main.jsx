f6f515bfc19deb40b94ea29d80177764b7fe28f0
BT01257 <devdwuti@gmail.com>
2025-11-24 18:30:34 +0530

    init components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import App from './App.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "home",
+    element: <div>Question list</div>,
+  },
+  {
+    path: "*",
+    element: <div>Non found</div>,
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
