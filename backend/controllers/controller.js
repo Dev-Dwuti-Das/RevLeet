34e088c34e6b16fc216462ceaef572fc5b466332
BT01257 <devdwuti@gmail.com>
2025-11-26 23:19:07 +0530

    add: question in frontend

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -3,7 +3,11 @@ import default_questions from "../models/default_questions.js";
 async function getquestions(req, res) {
   try {
     const questions_data = await default_questions.find({});
-    return res.status(200).json(questions_data);
+
+    return res
+    .setHeader("Access-Control-Allow-Origin",'http://localhost:5173')
+    .status(200)
+    .json(questions_data);
   } catch (err) {
     return res.status(500).json({ error: "something bad happend" });
   }
